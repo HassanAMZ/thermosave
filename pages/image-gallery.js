4464236e80ed8390d4c36e12d46cd0b4ac1fdbce
Shahzada Ali Hassan <59549481+HassanAMZ@users.noreply.github.com>
2022-06-20 20:13:37 +0500

    Updated ImageCaorsal Page

--- pages/image-gallery.js
+++ pages/image-gallery.js
@@ -2,6 +2,7 @@ import siteMetadata from '@/data/siteMetadata'
 import projectsData from '@/data/projectsData'
 import Card from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
+import ImageCarousel from '@/components/ImageCarousel'
 import { Container, Grid, GridItem } from '@chakra-ui/react'
 
 const ImageGallery = () => {
@@ -11,7 +12,9 @@ const ImageGallery = () => {
         title={`Image Gallery - ${siteMetadata.author}`}
         description={siteMetadata.description}
       />
-      <Container maxW="container.xl" p={{ base: '0', sm: 'auto' }}></Container>
+      <Container maxW="container.xl" p={{ base: '0', sm: 'auto' }}>
+        <ImageCarousel />
+      </Container>
     </>
   )
 }
