770b3f854d382fe1987a7f79b831a2f3bda40e1a
Shahzada Ali Hassan <59549481+HassanAMZ@users.noreply.github.com>
2022-06-23 18:20:43 +0500

    Updated SendAMessage Spacing

--- components/SendAMessage.js
+++ components/SendAMessage.js
@@ -7,9 +7,7 @@ export default function SendAMessage() {
       <Button
         rounded={'10'}
         w={'full'}
-        mt={8}
         size={'lg'}
-        py={'7'}
         bg={useColorModeValue('gray.900', 'gray.50')}
         color={useColorModeValue('white', 'gray.900')}
         leftIcon={<SiMessenger />}
